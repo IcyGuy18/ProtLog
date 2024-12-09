a56979b4090399343d0c4f06f87c6f07e2277844
= <=>
2024-12-10 00:06:39 +0500

    Updated documentation (completed)

--- server/static/docs.js
+++ server/static/docs.js
@@ -1,4 +1,4 @@
-function scrollIntoView(elementName) {
+function scrollToSection(elementName) {
     document.getElementById(elementName)
     .scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
